b3c1e23e376f0cae23b7696ca7df6ffcfe182952
Diego <a20191027@pucp.edu.pe>
2022-11-30 06:28:29 -0500

    Commit con vistasx

--- app/src/main/java/com/example/gropypucp/NuevaCuentaActivity.java
+++ app/src/main/java/com/example/gropypucp/NuevaCuentaActivity.java
@@ -2,7 +2,11 @@ package com.example.gropypucp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.gropypucp.Profesor.ListaEstudiantesProfesorActivity;
 
 public class NuevaCuentaActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class NuevaCuentaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_cuenta);
+    }
+    public void vista1(View view){
+        Intent intent = new Intent(this, ListaEstudiantesProfesorActivity.class);
+        startActivity(intent);
+
     }
 }
