3d08584f25e5acfad526629d7394475e642f4637
Joau0824 <a20182162@pucp.edu.pe>
2022-11-29 19:37:40 -0500

    mi segundo commit

--- app/src/main/java/com/example/gropypucp/InicioGropyActivity.java
+++ app/src/main/java/com/example/gropypucp/InicioGropyActivity.java
@@ -3,6 +3,7 @@ package com.example.gropypucp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class InicioGropyActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class InicioGropyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_gropy);
+    }
+
+    public void primeraVista (View view){
+
     }
 }
