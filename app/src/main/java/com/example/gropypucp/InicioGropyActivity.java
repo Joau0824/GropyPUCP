34fb7bbd606fdc30eecb81bc25119edd95defa48
Joau0824 <a20182162@pucp.edu.pe>
2022-11-29 19:13:36 -0500

    Primer Commit

---
+++ app/src/main/java/com/example/gropypucp/InicioGropyActivity.java
@@ -0,0 +1,14 @@
+package com.example.gropypucp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioGropyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_gropy);
+    }
+}
